2a35a438bfd6accaa7648bd7a7f4f5422f77b663
29sakshu <sakshis29091@gmail.com>
2024-10-20 18:29:39 +0300

    change the age

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,10 +1,11 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 10;
+        int age = 31;
+        // at the time of adult
         if (age > 0 && age < 18)
- {             System.out.println("You are underage."); }
-else {
-            System.out.println("You are an adult.");         }
+        { System.out.println("You are underage."); }
+        else { 
+           System.out.println("You are an adult."); } 
         System.out.println("Press space to exit.");
+        }
     }
-}
