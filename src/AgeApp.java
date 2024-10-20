3662b52a1555368e520fe7abbcd3e4666c3e4ab8
29sakshu <sakshis29091@gmail.com>
2024-10-20 18:06:14 +0300

    agebar

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,9 +1,9 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 11; // Change the age here to test
+        int age = 81; // Change the age here to test
 
         if (age > 0 && age < 18) {
-            
+          // print the age levels  
             System.out.println("You are underage.");
         } else if (age >= 65) {
             System.out.println("You are retired.");
