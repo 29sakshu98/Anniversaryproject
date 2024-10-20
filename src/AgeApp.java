a104ebcce19400a7b3f06eb63db348e7033b509c
29sakshu <sakshis29091@gmail.com>
2024-10-20 18:10:57 +0300

    done ages

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,15 +1,10 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 81; // Change the age here to test
-
-        if (age > 0 && age < 18) {
-          // print the age levels  
-            System.out.println("You are underage.");
-        } else if (age >= 65) {
-            System.out.println("You are retired.");
-        } else {
-            System.out.println("You are an adult.");
-        }
+        int age = 10;
+        if (age > 0 && age < 18)
+ {             System.out.println("You are underage."); }
+else {
+            System.out.println("You are an adult.");         }
         System.out.println("Press space to exit.");
     }
 }
